0c195be2e525cf37fdcb351498cc24739a809e45
Tanvir Faysal <t.faysal45@gmail.com>
2023-05-11 02:56:15 +0600

    Successfully done task two

--- react-task/src/components/Problem-2.jsx
+++ react-task/src/components/Problem-2.jsx
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllContactsModal from './AllContactsModal';
+import UsContactsModal from './UsContactsModal';
+
 
 const Problem2 = () => {
+
+    /**
+     * State section 
+    */
+
+    const [toggleAllContactsModal, setToggleAllContactsModal] = useState(false);
+    const [toggleUsContactsModal, setToggleUsContactsModal] = useState(false);
+    const [allContactsData, setAllContactsData] = useState([]);
+    const [usContactsData, setUsContactsData] = useState([]);
+
+    /**
+     * SideEffect for get data from restAPI start
+    */
+
+    useEffect(() => {
+        // fetch data
+        const dataFetch = async () => {
+            const items = await (
+                await fetch(
+                    "https://contact.mediusware.com/api/contacts/?page=1&page_size=10"
+                )
+            ).json();
+
+            // set state when the data received
+            setAllContactsData(items ? items.results : []);
+        };
+
+        const usDataFetch = async () => {
+            const items = await (
+                await fetch(
+                    "https://contact.mediusware.com/api/country-contacts/United%20States/?page=1&page_size=10"
+                )
+            ).json();
+
+            // set state when the data received
+            setUsContactsData(items ? items.results : []);
+        };
+
+        usDataFetch();
+        dataFetch();
+    }, []);
+
+    /**
+     * SideEffect for get data from restAPI end
+    */
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button className="btn btn-lg btn-outline-primary" type="button" onClick={() => setToggleAllContactsModal(true)} >All Contacts</button>
+                    <button className="btn btn-lg btn-outline-warning" type="button" onClick={() => setToggleUsContactsModal(true)}>US Contacts</button>
                 </div>
-                
+                <AllContactsModal show={toggleAllContactsModal} showAnother={setToggleUsContactsModal} setShow={setToggleAllContactsModal} data={allContactsData} />
+                <UsContactsModal show={toggleUsContactsModal} showAnother={setToggleAllContactsModal} setShow={setToggleUsContactsModal} data={usContactsData} />
             </div>
         </div>
     );
---
+++ react-task/src/components/UsContactsModal.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const UsContactsModal = ({ show, setShow, data, showAnother }) => {
+
+  /**
+   * State section 
+  */
+
+  const modalClose = () => setShow(false);
+  const [checkEven, setCheckEven] = useState(false);
+
+  /**
+   * Even handler function
+  */
+
+  const handleChangeEven = (checked) => {
+    setCheckEven(checked);
+  }
+
+  /**
+   * Handle change modal functionality
+  */
+
+  const handleSwitch = () => {
+    showAnother(true);
+    modalClose();
+  }
+
+
+  return (
+    <div>
+      <Modal show={show} onHide={modalClose} centered>
+        <Modal.Header closeButton>
+          <Button onClick={handleSwitch} variant="primary" className="mx-2">
+            All Contacts
+          </Button>
+          <Button variant="primary" className="mx-2">
+            Us Contacts
+          </Button>
+        </Modal.Header>
+        <Modal.Body>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Country</th>
+                <th scope="col">Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 &&
+                data.map((item, index) => (
+                  <tr key={index}>
+                    {
+                      checkEven ? item.id % 2 === 0 &&
+                        <>
+                          <td>{item.id}</td>
+                          <td>{item.country.name}</td>
+                          <td>{item.phone}</td>
+                        </> :
+                        <>
+                          <td>{item.id}</td>
+                          <td>{item.country.name}</td>
+                          <td>{item.phone}</td>
+                        </>
+                    }
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </Modal.Body>
+        <div className="float-left">
+          <label className="mx-3">Only Even</label>
+          <input onChange={(e) => handleChangeEven(e.target.checked)} type="checkbox" />
+        </div>
+        <Modal.Footer>
+          <Button variant="danger" onClick={modalClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default UsContactsModal;
